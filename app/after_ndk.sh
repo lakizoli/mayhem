40993c462e7d7e4d4d8ca2a85ffa873a9cec1072
Laki, Zoltan <ZLaki@graphisoft.com>
2016-01-21 11:34:44 +0100

    megy az osx build es debug

--- app/after_ndk.sh
+++ app/after_ndk.sh
@@ -1,2 +1,1 @@
-#/bin/sh
-find build/intermediates/ndk/obj/local/* -iname "*.so" | cut -sd / -f 6- | xargs -d '\n' -I % cp -Rv build/intermediates/ndk/obj/local/% src/main/jniLibs/%
+find build/intermediates/ndk/obj/local/* -iname "*.so" | cut -sd / -f 6- | xargs -I{} cp -Rv build/intermediates/ndk/obj/local/{} src/main/jniLibs/{}
