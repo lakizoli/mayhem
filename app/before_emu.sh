9f39ecf1710ae146a9d1f1ddbd6a5218b25866a2
Laki, Zoltan <ZLaki@graphisoft.com>
2016-01-25 16:07:23 +0100

    Android Studio makefile javitasok (mar megy parancssorbol is, ill. Android Studio-bol is osx-en)

--- app/before_emu.sh
+++ app/before_emu.sh
@@ -2,3 +2,4 @@ mv ../../vice-2.4-android/src/arch/win32/msvc/inttypes.h ../../vice-2.4-android/
 mv ../../vice-2.4-android/src/arch/win32/msvc/stdint.h ../../vice-2.4-android/src/arch/win32/msvc/__stdint.h
 mv ../../vice-2.4-android/src/arch/win32/msvc/dirent.h ../../vice-2.4-android/src/arch/win32/msvc/__dirent.h
 mv ../../vice-2.4-android/src/arch/win32/msvc/msvc_ver.h ../../vice-2.4-android/src/arch/win32/msvc/__msvc_ver.h
+exit 0
