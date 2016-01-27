7f327d6a183a265bffe77374509c6f0510d90cec
lakizoli <lakizoli@yahoo.com>
2016-01-27 23:19:23 +0100

    gradle magia

---
+++ app/after_emu.cmd
@@ -0,0 +1,4 @@
+rename ..\..\vice-2.4-android\src\arch\win32\msvc\__inttypes.h inttypes.h
+rename ..\..\vice-2.4-android\src\arch\win32\msvc\__stdint.h stdint.h
+rename ..\..\vice-2.4-android\src\arch\win32\msvc\__dirent.h dirent.h
+rename ..\..\vice-2.4-android\src\arch\win32\msvc\__msvc_ver.h msvc_ver.h 
