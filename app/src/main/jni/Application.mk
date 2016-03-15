d71787918dc85dd39902a17430fe99b812559572
zoli <lakizoli@yahoo.com>
2016-03-15 11:35:10 +0100

    gcc warning-ok kiszedese

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,5 +2,5 @@ APP_PLATFORM := android-10
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_MODULES := game
 APP_STL := c++_shared
-APP_CFLAGS := -Wno-error=format-security -Wno-multichar -Wno-deprecated-register $(GAME_CFLAGS)
+APP_CFLAGS := -Wno-error=format-security -Wno-multichar $(GAME_CFLAGS)
 APP_CPPFLAGS := -Wno-error=format-security -Wno-format-security --rtti -fexceptions $(GAME_CPPFLAGS)
